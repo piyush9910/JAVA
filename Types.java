4eada319fd05d8dea5d48c96c04f0f32b5f2e542
piyush9910 <54396900+piyush9910@users.noreply.github.com>
2020-01-22 13:09:45 +0530

    Add files via upload

---
+++ Types.java
@@ -0,0 +1,29 @@
+public class Types
+{
+   public static void main(String args[])
+   {
+     char c='x';
+     byte b=50;
+     short s=1996;
+     int i=12345;
+     long l=57658764878466L;
+     double d=567788.7897898;
+     float f=1.2f;
+     
+   System.out.println("Char is "+c);
+    System.out.println("Byte is "+b);
+    System.out.println("Short is "+s);
+    System.out.println("integer is "+i);
+    System.out.println("long is "+l);
+     System.out.println("double is "+d);
+     System.out.println("float is "+f);
+    int x=(int)f;
+    System.out.println(x);
+
+   float z=(float)i;
+   System.out.println("z is "+z);
+  }
+}
+
+
+   
